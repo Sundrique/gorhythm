35f1bdabf807f086543e34a8bafc16d551a4cbb4
Aleksandr Sandrovsk <asandrovsky@gmail.com>
2015-03-22 15:30:52 +0700

    More generic BST accepting different types of values

--- btree.go
+++ btree.go
@@ -1,27 +1,91 @@
 package gorhythm
 
+type Comparable interface {
+	Greater(value interface{}) bool
+}
+
+func NewNode(value interface{}) *Node {
+	return &Node{Value: value, initialized: true}
+}
+
 type Node struct {
-	Value int
-	Left  *Node
-	Right *Node
+	Value       interface{}
+	left        *Node
+	right       *Node
+	initialized bool
 }
 
-func NewNode(value int) *Node {
-	return &Node{value, nil, nil}
+func (n *Node) Left() *Node {
+	return n.left
 }
 
-func (n *Node) Insert(value int) {
-	if value-n.Value <= 0 {
-		if n.Left == nil {
-			n.Left = NewNode(value)
+func (n *Node) Right() *Node {
+	return n.right
+}
+
+func (n *Node) Insert(value interface{}) *Node {
+	var newNode *Node
+
+	if n != nil {
+		if n.Value == nil {
+			n.Value = value
+			newNode = n
 		} else {
-			n.Left.Insert(value)
+			if n.Greater(value) {
+				n.left = n.left.Insert(value)
+				newNode = n.left
+			} else {
+				n.right = n.right.Insert(value)
+				newNode = n.right
+			}
 		}
 	} else {
-		if n.Right == nil {
-			n.Right = NewNode(value)
-		} else {
-			n.Right.Insert(value)
-		}
+		newNode = &Node{Value: value}
+	}
+
+	return newNode
+}
+
+func (n *Node) IsNil() bool {
+	return !n.initialized
+}
+
+func (n *Node) init(value interface{}) {
+	n.Value = value
+	n.initialized = true
+}
+
+func (n *Node) Greater(value interface{}) bool {
+	switch n.Value.(type) {
+	default:
+		return false
+	case Comparable:
+		return n.Value.(Comparable).Greater(value)
+	case int:
+		return n.Value.(int) > value.(int)
+	case int8:
+		return n.Value.(int8) > value.(int8)
+	case int16:
+		return n.Value.(int16) > value.(int16)
+	case int32:
+		return n.Value.(int32) > value.(int32)
+	case int64:
+		return n.Value.(int64) > value.(int64)
+	case uint:
+		return n.Value.(uint) > value.(uint)
+	case uint8:
+		return n.Value.(uint8) > value.(uint8)
+	case uint16:
+		return n.Value.(uint16) > value.(uint16)
+	case uint32:
+		return n.Value.(uint32) > value.(uint32)
+	case uint64:
+		return n.Value.(uint64) > value.(uint64)
+	case float32:
+		return n.Value.(float32) > value.(float32)
+	case float64:
+		return n.Value.(float64) > value.(float64)
+	case string:
+		return n.Value.(string) > value.(string)
 	}
 }
--- btree_test.go
+++ btree_test.go
@@ -9,27 +9,32 @@ type BTreeSuite struct{}
 var _ = Suite(&BTreeSuite{})
 
 func (s *BTreeSuite) TestNodeConstructor(c *C) {
-	node := NewNode(1)
+
+	var node Node
+
+	node.Insert(1)
 
 	c.Check(node.Value, Equals, 1)
-	c.Check(node.Right, IsNil)
-	c.Check(node.Left, IsNil)
+	c.Check(node.Right(), IsNil)
+	c.Check(node.Left(), IsNil)
 }
 
 func (s *BTreeSuite) TestInsert(c *C) {
-	tree := NewNode(3)
+	var tree Node
+
+	tree.Insert(3)
 
 	c.Check(tree.Value, Equals, 3)
-	c.Check(tree.Right, IsNil)
-	c.Check(tree.Left, IsNil)
+	c.Check(tree.Right(), IsNil)
+	c.Check(tree.Left(), IsNil)
 
 	tree.Insert(2)
 
-	c.Assert(tree.Left, Not(IsNil))
-	c.Check(tree.Left.Value, Equals, 2)
+	c.Assert(tree.Left(), Not(IsNil))
+	c.Check(tree.Left().Value, Equals, 2)
 
 	tree.Insert(4)
 
-	c.Assert(tree.Right, Not(IsNil))
-	c.Check(tree.Right.Value, Equals, 4)
+	c.Assert(tree.Right(), Not(IsNil))
+	c.Check(tree.Right().Value, Equals, 4)
 }
